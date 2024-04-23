ae0ebabfa60d6b930e611c9a7dd994824dfb0e72
partha <partharaj.dev@gmail.com>
2024-04-24 00:27:27 +0600

    Added Swagger UI to the project.

--- CQRS.Command.Api/Program.cs
+++ CQRS.Command.Api/Program.cs
@@ -1,5 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(setupAction =>
+{
+    setupAction.SwaggerDoc("BankManagement", new()
+    {
+        Title = "Command Query Responsibility Segregation",
+        Version = "1"
+    });
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(
+        setupAction =>
+        {
+            setupAction.SwaggerEndpoint("swagger/BankManagement/swagger.json", "Bank Management API");
+            setupAction.RoutePrefix = string.Empty;
+        });
+}
 
 app.MapGet("/", () => "Hello World!");
 
